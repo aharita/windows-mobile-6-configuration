6b3626548218fabf043e4df4c41298760fe37c25
unknown <aharita@NSL-AHARITA.nearsoft.local>
2012-04-28 18:36:55 -0700

    added configuration and directory helpers added demo project

---
+++ WindowsMobileUtils/ConfigurationHelper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+
+namespace WindowsMobileUtils
+{
+    public class ConfigurationHelper
+    {
+        #region Properties
+        private static readonly ConfigurationHelper _instance = new ConfigurationHelper();
+        public static ConfigurationHelper Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        private Dictionary<string, string> _settings = new Dictionary<string, string>();
+        public Dictionary<string, string> Settings
+        {
+            get
+            {
+                return _settings;
+            }
+        }
+        #endregion
+
+        #region Constructors
+        static ConfigurationHelper()
+        {
+
+        }
+
+        private ConfigurationHelper()
+        {
+            XDocument doc = XDocument.Load(Path.Combine(DirectoryHelper.CurrentDirectory, "Configuration.xml"));
+
+            foreach (XElement element in doc.Descendants("setting"))
+            {
+                this.Settings.Add(element.Attribute("name").Value, element.Attribute("value").Value);
+            }
+        }
+        #endregion
+    }
+}
---
+++ WindowsMobileUtils/DirectoryHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace WindowsMobileUtils
+{
+    public class DirectoryHelper
+    {
+        public static readonly string CurrentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase);
+    }
+}
